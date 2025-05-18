3586c8e891bbadcabd559618e9cbeb4c9afbb277
programmerShoaib <link2shoaib.2002@gmail.com>
2025-05-18 21:09:06 +0500

    add animation while the data is being fetched

--- fetching-list-of-user-using-API/script.js
+++ fetching-list-of-user-using-API/script.js
@@ -11,26 +11,29 @@ document.addEventListener("DOMContentLoaded", () => {
         renderUsers(users);
     });
 
-    function renderUsers(users) {
-        users.forEach((user) => {
+    function renderUsers(users) {  // function to render the users
+        users.forEach((user) => { 
             console.log(user);
             const tr = document.createElement("tr");
             tr.classList.add("mb-2", "border", "border-gray-300", "p-2");
             tr.innerHTML = `<td>${user.id}</td>  <td>${user.name}</td>  <td>${user.email}</td> <td>${user.phone}</td>`;
-            userList.appendChild(tr);
+            userList.appendChild(tr);  // append the user to the userList
         });
     }
 
     async function fetchUsers() {
         try {
-            const response = await fetch(url);
+            userDiv.classList.add("hidden");   // hide the userDiv
+            loader.classList.remove("hidden");  // show the loader
+            await new Promise((resolve) => setTimeout(resolve, 3000)); // wait for 3 seconds
+            const response = await fetch(url);   // fetch the data
             if (!response.ok) {
                 throw new Error("Network response was not ok");
             }
-            const data = await response.json();
-            loader.classList.add("hidden");
-            userDiv.classList.add("hidden");
-            userListDiv.classList.remove("hidden");
+            const data = await response.json();  // parse the data
+            loader.classList.add("hidden");   // hide the loader
+            userDiv.classList.add("hidden");  // hide the userDiv
+            userListDiv.classList.remove("hidden");  // show the userListDiv
             return data;
         } catch (error) {
             console.error("Error fetching users:", error);
